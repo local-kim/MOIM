3d7526488344e386db0ddd19634bbec6ffcd2993
김현지 <gumgum28@naver.com>
2022-10-18 23:38:36 +0900

    Initial commit

---
+++ src/main/java/com/project/cobell/CobellApplication.java
@@ -0,0 +1,13 @@
+package com.project.cobell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CobellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CobellApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/cobell/CobellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.cobell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CobellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
