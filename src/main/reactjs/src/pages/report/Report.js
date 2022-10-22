2ae12cffaa8ab7449ce42821ddbd761dcce177f4
김현지 <gumgum28@naver.com>
2022-10-22 19:05:40 +0900

    로그인 여부에 따라 로그인/로그아웃 버튼 표시

--- src/main/reactjs/src/pages/report/Report.js
+++ src/main/reactjs/src/pages/report/Report.js
@@ -1,15 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 const Report = () => {
   const navigate = useNavigate();
+  const [loggedIn, setLoggedIn] = useState(localStorage.getItem("user") ? true : false);
 
   return (
     <div>
       <h1>메인 페이지</h1>
-
-      <button type='button' onClick={() => navigate('/login')}>로그인</button>
-      <button type='button' onClick={() => navigate('/join')}>회원가입</button>
+      {
+        loggedIn && <button type='button' onClick={() => {
+          localStorage.removeItem("user");
+          setLoggedIn(false);
+        }}>로그아웃</button>
+      }
+      {
+        !loggedIn && 
+        <div>
+          <button type='button' onClick={() => navigate('/login')}>로그인</button>
+          <button type='button' onClick={() => navigate('/join')}>회원가입</button>
+        </div>
+      }
 
       <button type='button' onClick={() => navigate('/lounge')}>라운지(챌린지 목록)</button>
 
